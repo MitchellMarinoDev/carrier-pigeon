f48fe1a1dccc49c4c5701e86d9bda55673cbf52a
Mitchell Marino <marinodev@tuta.io>
2022-04-25 21:19:31 -0500

    doc issue

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,6 @@
 //! and deserialization. This way you can worry about what to send, and pigeon will worry
 //! about how to send it.
 //!
-//! ### Add carrier-pigeon to your `Cargo.toml`:
-//! ```
-//! carrier-pigeon = "0.3.0"
-//! ```
-//!
 //! ## Examples
 //!
 //! Complete examples are provided in the
